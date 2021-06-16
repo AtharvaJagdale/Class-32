93bef293511b2dfd74b72e686f3b29fab875476a
AtharvaJagdale <atharvajagdale0207@gmail.com>
2021-06-16 16:28:57 +0530

    Class 32

--- Slingshot.js
+++ Slingshot.js
@@ -26,11 +26,19 @@ class SlingShot{
             var pointA = this.Slingshot.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-
+            if(pointA.x<205){
+            line(pointA.x-20, pointA.y, pointB.x-20,pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+20,pointB.y);
            
-            
-          
+            image(this.sling3,pointA.x-25,pointA.y-20,20,30)
+        }
+          else {
+            line(pointA.x+20, pointA.y, pointB.x-20,pointB.y);
+            line(pointA.x+20, pointA.y, pointB.x+20,pointB.y);
+           
+            image(this.sling3,pointA.x+25,pointA.y-20,20,30)
+
+          }
         }
     }
     
